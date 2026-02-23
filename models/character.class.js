0ba34c94ca7add81da4d6b95ff4e634e7836aa23
EtemSakin <etemsakin66@gmail.com>
2026-02-23 11:42:32 +0100

    backgrounds added und some fixes

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,10 @@
 class Character extends MovableObject {
 
+    y = 170;
+    height = 280;
+    width = 150;
+
+
     constructor() {
         super();this.loadImage('img/2_character_pepe/2_walk/W-21.png');
     }
--- models/world.class.js
+++ models/world.class.js
@@ -8,6 +8,14 @@ class World {
     clouds = [
         new Cloud(),
     ];
+    backgroundObjects = [
+        new BackgroundObject('img/5_background/layers/air.png', 0),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
+        
+        
+    ];
     canvas;
     ctx;
 
@@ -19,21 +27,27 @@ class World {
 
     draw() {
 
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);    
 
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
-        this.enemies.forEach(enemy => {
-            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
-        });
 
-        this.clouds.forEach(cloud => {
-            this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
-        });
+        this.addObjectstoMap(this.backgroundObjects);
+       this.addtoMap(this.character); 
+       this.addObjectstoMap(this.enemies);
+       this.addObjectstoMap(this.clouds);
 
-        
         let self = this;
         requestAnimationFrame(function() {
             self.draw();
         });
+    }
+
+    addObjectstoMap(objects) {
+        objects.forEach(o => {
+            this.addtoMap(o);
+        });
+    }
+
+    addtoMap(mO) {
+        this.ctx.drawImage(mO.img, mO.x, mO.y, mO.width, mO.height);
     }
 }
