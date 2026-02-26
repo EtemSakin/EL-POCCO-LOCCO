15ba599e255c89fffd8bc46303223ad4981d8fcd
EtemSakin <etemsakin66@gmail.com>
2026-02-26 10:23:19 +0100

    jump animation adeed

--- models/character.class.js
+++ models/character.class.js
@@ -1,52 +1,89 @@
 class Character extends MovableObject {
+  y = 80;
+  height = 250;
+  width = 120;
+  speed = 5;
 
-    y = 80;
-    height = 250;
-    speed = 5;
-    IMAGES_WALKING = [
-        'img/2_character_pepe/2_walk/W-21.png',
-        'img/2_character_pepe/2_walk/W-22.png',
-        'img/2_character_pepe/2_walk/W-23.png',
-        'img/2_character_pepe/2_walk/W-24.png',
-        'img/2_character_pepe/2_walk/W-25.png',
-        'img/2_character_pepe/2_walk/W-26.png',
-    ];
-    world;
-    curentImage = 0;
+  IMAGES_WALKING = [
+    "img/2_character_pepe/2_walk/W-21.png",
+    "img/2_character_pepe/2_walk/W-22.png",
+    "img/2_character_pepe/2_walk/W-23.png",
+    "img/2_character_pepe/2_walk/W-24.png",
+    "img/2_character_pepe/2_walk/W-25.png",
+    "img/2_character_pepe/2_walk/W-26.png",
+  ];
 
+  IMAGES_JUMPING = [
+    "img/2_character_pepe/3_jump/J-31.png",
+    "img/2_character_pepe/3_jump/J-32.png",
+    "img/2_character_pepe/3_jump/J-33.png",
+    "img/2_character_pepe/3_jump/J-34.png",
+    "img/2_character_pepe/3_jump/J-35.png",
+    "img/2_character_pepe/3_jump/J-36.png",
+    "img/2_character_pepe/3_jump/J-37.png",
+    "img/2_character_pepe/3_jump/J-38.png",
+    "img/2_character_pepe/3_jump/J-39.png",
+  ];
 
+  world;
 
-    constructor() {
-        super();this.loadImage('img/2_character_pepe/2_walk/W-21.png');
-        super.loadImages(this.IMAGES_WALKING);
-        this.applyGravity();
+  constructor() {
+    super();
+    this.loadImage(this.IMAGES_WALKING[0]);
+    this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.IMAGES_JUMPING);
 
-        this.animate();
+    this.groundY = 170;
+
+    this.applyGravity();
+  }
+
+  setWorld(world) {
+    this.world = world;
+  }
+
+  animate() {
+    setInterval(() => {
+      if (!this.world || !this.world.keyboard) return;
+
+      const levelEnd =
+        this.world.level && this.world.level.level_end_x !== undefined
+          ? this.world.level.level_end_x
+          : Infinity;
+
+      const movingRight = this.world.keyboard.RIGHT && this.x < levelEnd;
+      const movingLeft = this.world.keyboard.LEFT && this.x > 0;
+
+      if (movingRight) {
+        this.x += this.speed;
+        this.otherDirection = false;
+      }
+
+      if (movingLeft) {
+        this.x -= this.speed;
+        this.otherDirection = true;
+      }
+
+      if (this.world) {
+        this.world.camera_x = -this.x + 100;
+      }
+    }, 1000 / 60);
+
+    setInterval(() => {
+      if (!this.world || !this.world.keyboard) return;
+
+      if (this.isAboveGround()) {
+      } else {
+        if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+          this.playAnimation(this.IMAGES_WALKING);
+        }
+      }
+    }, 80);
+  }
+
+  jump() {
+    if (!this.isAboveGround()) {
+      this.speedY = 15;
     }
-
-    animate() {
-
-        setInterval(() => {
-            if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
-                this.x += this.speed;
-                this.otherDirection = false;
-            }
-                if (this.world.keyboard.LEFT && this.x > 0) {
-                this.x -= this.speed;
-                this.otherDirection = true;
-            }
-            this.world.camera_x = -this.x + 100;
-        }, 500 / 60);
-
-        setInterval(() => {
-
-            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-                this.playAnimation(this.IMAGES_WALKING);
-            }
-        }, 50);
-    }    
-
-    jump() {
-
-    }
+  }
 }
--- models/world.class.js
+++ models/world.class.js
@@ -1,58 +1,61 @@
-class World { 
-    character = new Character();
-    level = level1;
-    canvas;
-    ctx;
-    keyboard;
-    camera_x = 0;
+class World {
+  character = new Character();
+  level = level1;
 
-    constructor(canvas, keyboard) {
-        this.ctx = canvas.getContext('2d');
-        this.canvas = canvas;
-        this.keyboard = keyboard;
-        this.draw();
-        this.setWorld();
+  canvas;
+  ctx;
+  keyboard;
+  camera_x = 0;
+
+  constructor(canvas, keyboard) {
+    this.ctx = canvas.getContext("2d");
+    this.canvas = canvas;
+    this.keyboard = keyboard;
+
+    this.setWorld(); // zuerst
+    this.draw(); // dann
+  }
+
+  setWorld() {
+    this.character.world = this;
+    this.character.animate();
+  }
+
+  draw() {
+    // Reset Transform, damit nichts driftet
+    this.ctx.setTransform(1, 0, 0, 1, 0, 0);
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    // Kamera
+    this.ctx.translate(this.camera_x, 0);
+
+    this.addObjectstoMap(this.level.backgroundObjects);
+    this.addtoMap(this.character);
+    this.addObjectstoMap(this.level.enemies);
+    this.addObjectstoMap(this.level.clouds);
+
+    requestAnimationFrame(() => this.draw());
+  }
+
+  addObjectstoMap(objects) {
+    objects.forEach((o) => this.addtoMap(o));
+  }
+
+  addtoMap(mO) {
+    if (!mO.img || !mO.img.complete || mO.img.naturalWidth === 0) return;
+
+    if (mO.otherDirection) {
+      this.ctx.save();
+      this.ctx.translate(mO.width, 0);
+      this.ctx.scale(-1, 1);
+      mO.x = mO.x * -1;
     }
 
-    setWorld() {
-        this.character.world = this;
+    this.ctx.drawImage(mO.img, mO.x, mO.y, mO.width, mO.height);
+
+    if (mO.otherDirection) {
+      mO.x = mO.x * -1;
+      this.ctx.restore();
     }
-
-    draw() {
-
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.ctx.translate(this.camera_x, 0);  
-
-
-        this.addObjectstoMap(this.level.backgroundObjects);
-       this.addtoMap(this.character); 
-       this.addObjectstoMap(this.level.enemies);
-       this.addObjectstoMap(this.level.clouds);
-       this.ctx.translate(-this.camera_x, 0);
-
-        let self = this;
-        requestAnimationFrame(function() {
-            self.draw();
-        });
-    }
-
-    addObjectstoMap(objects) {
-        objects.forEach(o => {
-            this.addtoMap(o);
-        });
-    }
-
-    addtoMap(mO) {
-        if (mO.otherDirection) {
-            this.ctx.save();
-            this.ctx.translate(mO.width, 0);
-            this.ctx.scale(-1, 1);
-            mO.x = mO.x * -1;
-        }
-        this.ctx.drawImage(mO.img, mO.x, mO.y, mO.width, mO.height);
-        if (mO.otherDirection) {
-            mO.x = mO.x * -1;
-            this.ctx.restore();
-        }
-    }
+  }
 }
